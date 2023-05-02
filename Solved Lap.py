5c368ab283072886d3f8fffe66dbb46c8e7ce5c6
Rayan Hantoul <ryan-055@outlook.com>
2023-05-02 15:01:47 +0300

    Finshed Lap

---
+++ Solved Lap.py
@@ -0,0 +1,25 @@
+price = 20
+
+quantity = 5
+
+tax_rat = 0.15
+
+subtotal = (price * quantity)
+
+tax = (subtotal * tax_rat)
+
+total = (subtotal + tax)
+
+print("price of item: SAR",price)
+
+print('quantity:',quantity)
+
+print('tax:',tax_rat)
+
+print('_'*30)
+
+print('subtotal:',subtotal)
+
+print('tax:',tax)
+
+print('total',total)
